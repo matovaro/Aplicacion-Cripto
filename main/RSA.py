e95c58640cb03ac49ea34e48660a9221cbd5ce39
Miguel Tovar <matovaro@unal.edu.co>
2018-12-10 09:25:47 -0500

    'Modificaciones'

--- main/RSA.py
+++ main/RSA.py
@@ -1,9 +1,6 @@
 import math
 import random
 
-
-print('################################# RSA')
-
 def gcd(a,b):
     if(b==0):
         return a
@@ -65,114 +62,32 @@ def ver(y,e,n,m):
         return True
     else:
         return False
-########################################################################################################################
-########################################################################################################################
-########################################################################################################################
-# m=6882326879666683
-# print('m=',end=' ')
-# print(m)
-# p=1009
-# print('p=',end=' ')
-# print(p)
-# q=1013
-# print('q=',end=' ')
-# print(q)
-#
-# mens_array=[]
-#
-# stri=str(m)
-# for i in range(math.ceil(len(stri)/3)):
-#     start=i*3
-#     end=(i+1)*3
-#     mens_array.append(int(stri[start:end]))
-#
-# print('Mensaje array=',end=' ')
-# print(m)
-# n=p*q
-# print('n=',end=' ')
-# print(n)
-#
-# phi=(p-1)*(q-1)
-# print('phi=',end=' ')
-# print(phi)
-#
-#
-#
-# d=-1;
-# while(d < 0):
-#     e = random.randint(1, phi)
-#     while(gcd(e,phi) != 1):
-#         e = random.randint(1, phi)
-#
-#
-#
-#     #print(gcd(e,phi))
-#
-#     EA = EEA(phi,e)
-#
-#     d = EA[2]
-#
-# print('e =')
-# print(e)
-# print('EEA =',end=' ')
-# print(EA)
-# print('d =',end=' ')
-# print(d)
-#
-# print('Encript')
-# crip_array=[]
-# for i in range(len(mens_array)):
-#    cr = powermod(mens_array[i],e,n)
-#    crip_array.append(cr)
-# print(crip_array)
-#
-# print('Decript')
-# messa_array=[]
-# for i in range(len(crip_array)):
-#    me = powermod(crip_array[i],d,n)
-#    messa_array.append(me)
-# print(messa_array)
 
-########################################################################################################################
-########################################################################################################################
-########################################################################################################################
+def getE(lim):
+    return random.randint(1, lim)
 
-
-m=6882326879666683
-print('m=',end=' ')
-print(m)
+def breakArray(stri):
+    men_arr=[]
+    for i in range(math.ceil(len(stri) / 3)):
+        start = i * 3
+        end = (i + 1) * 3
+        men_arr.append(int(stri[start:end]))
+    return  men_arr
 p=104723
-print('p=',end=' ')
-print(p)
 q=104729
-print('q=',end=' ')
-print(q)
 
-mens_array=[]
 
-stri=str(m)
-for i in range(math.ceil(len(stri)/3)):
-    start=i*3
-    end=(i+1)*3
-    mens_array.append(int(stri[start:end]))
-
-print('Mensaje array=',end=' ')
-print(m)
 n=p*q
-print('n=',end=' ')
-print(n)
 
 phi=(p-1)*(q-1)
-print('phi=',end=' ')
-print(phi)
 
 
 
 d=-1;
 while(d < 0):
-    e = random.randint(1, phi)
+    e = getE(phi)
     while(gcd(e,phi) != 1):
-        e = random.randint(1, phi)
+        e = getE(phi)
 
 
 
@@ -182,12 +97,24 @@ while(d < 0):
 
     d = EA[2]
 
-print('e =')
-print(e)
-print('EEA =',end=' ')
-print(EA)
-print('d =',end=' ')
-print(d)
+
+def prE():
+    return e
+def prD():
+    return d
+
+
+
+m=6882326879666683
+print('m=',end=' ')
+print(m)
+mens_array=[]
+
+st=str(m)
+
+mens_array = breakArray(st);
+print('Mensaje array=',end=' ')
+print(mens_array)
 
 print('Sign')
 crip_array=sign(mens_array,d,n)
@@ -196,5 +123,9 @@ print(crip_array)
 print('Verification')
 messa_array=ver(crip_array,e,n,m)
 print(messa_array)
+
+
+print(prE())
+print(prD())
 
 
